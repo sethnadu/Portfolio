b5612e5c16edfca47621d49351f335082cd3165a
Seth Nadu <seth.nadu@gmail.com>
2019-07-11 19:08:04 -0400

    nav bar

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -1,28 +1,27 @@
 let navImage = document.querySelector(".navImg")
-let navImage2 = document.querySelector(".navImg2");
+// let navImage2 = document.querySelector(".navImg2");
+let navMobile = document.querySelector(".navMobile");
 let display = document.querySelector(".display");
 let navItem = document.querySelectorAll(".display a");
 let img = document.querySelectorAll(".navMobile img");
 
-// img.forEach(item => {
-//     item.addEventListener("click", () => {
-//         item.classList.toggle("change");
-//     })
-// })
+let navItemArray = Array.from(navItem);
 
 
 navImage.addEventListener("click", () => {
-display.classList.toggle("hide");
-navImage2.classList.toggle('hide');
-navImage.classList.toggle('hide');
+display.classList.toggle("expand");
+navItemArray.forEach(item => {
+    item.classList.toggle('hide');
+})
+
 // navImage.classList.toggle("change");
 
 })
 
-navImage2.addEventListener("click", () => {
-    display.classList.toggle("hide");
-    navImage.classList.toggle('hide');
-    navImage2.classList.toggle('hide');
-    // navImage2.classList.toggle("change");
-})
+// navImage2.addEventListener("click", () => {
+//     display.classList.toggle("hide");
+//     navImage.classList.toggle('hide');
+//     navImage2.classList.toggle('hide');
+//     // navImage2.classList.toggle("change");
+// })
 
