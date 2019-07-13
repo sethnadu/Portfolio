be2dd09c587cc51e4cb0ef33642266bfad4cff26
Seth Nadu <seth.nadu@gmail.com>
2019-07-13 12:05:23 -0400

    seth-nadu

--- JavaScript/box.js
+++ JavaScript/box.js
@@ -10,3 +10,35 @@ Array.from(boxes).forEach(box => {
         boxContent.style.display = "none";
     }) 
 })
+// Icon color change mobile on mouseover
+const icon = document.querySelectorAll("i")
+const fas = document.querySelector(".fas");
+const aboutIcons = document.querySelectorAll(".aboutIcons");
+const iconContent = document.querySelectorAll(".iconContent")
+const aboutSectionDiv = document.querySelector(".aboutSection");
+
+aboutIcons.forEach(word => {
+
+word.addEventListener("mouseover", () => {
+    icon.forEach(item => {
+        item.style.color = "rgba(78, 76, 76)"
+    })
+    iconContent.forEach(item => {
+        item.style.color = "rgba(78, 76, 76)"
+    })
+   
+})
+
+word.addEventListener("mouseleave", () => {
+    icon.forEach(item => {
+        item.style.color = "indianred"
+    })
+    iconContent.forEach(item => {
+        item.style.color = "black"
+    })
+}) 
+
+})
+
+
+
